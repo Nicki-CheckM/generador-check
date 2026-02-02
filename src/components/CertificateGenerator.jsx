1ab8de9875c826c2e1796429f19e35d1a721892b
Nicki-CheckM <nicoletteahumada1997@gmail.com>
2026-02-02 19:41:53 -0300

    Update CertificateGenerator.jsx

--- src/components/CertificateGenerator.jsx
+++ src/components/CertificateGenerator.jsx
@@ -307,6 +307,9 @@ const Certificate = ({ data }) => {
                 <Image style={styles.certificateImage} src={certificadoImage} />
               </View>
               <View style={styles.signatureContainer4}>
+                <Image style={styles.certhiaImage} src={certhiaImage} />
+              </View>
+              <View style={styles.signatureContainer4}>
                 {data.qrCodeUrl && (
                   <View style={styles.qrContainer}>
                     <Image src={data.qrCodeUrl} style={styles.qrImage} />
